abb737920392337943e48d5fae82836a2c68ccd4
qiuzh <qzh@ancent.cn>
2021-04-09 10:49:06 +0800

    构建Travis CI

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ if __name__ == '__main__':
     # 生成号段
     # 查询号段信息
     ghw = GouHaoWang()
-    for number in range(155, 156):
-        infos = ghw.get_info(str(number))
-        print(f"{number}数量： {len(infos)}")
+    ghw.search(number_seg="1353910")
+    # for number in range(155, 156):
+    #     infos = ghw.get_info(str(number))
+    #     print(f"{number}数量： {len(infos)}")
